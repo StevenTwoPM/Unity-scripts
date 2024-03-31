8cfa9f6e28ffbaffd6beac699e34daa42ea1d02a
StevenTwoPM <165568055+StevenTwoPM@users.noreply.github.com>
2024-03-31 15:51:50 +0200

    Add files via upload

---
+++ KlabiAhmedTalking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KlabiAhmedTalking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Bonjour c'est Klabi ahmed.");
+        print("SDIFESJFDODSFJSDFOSEJFDSOFESOFSDJFSODFSEOFOSFDSJFESOFPJSDFESIJFDSIFOEOSFJSDJFEOSFJSDFO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
